2cfcc368d8bb5812955e109ac96cf9552a470813
万峰 <iuliao@163.com>
2018-10-25 16:57:32 +0800

    第四次提交

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,10 +22,10 @@ module.exports = {
       user : 'yueyagou',
       host : '47.107.74.126',
       ref  : 'origin/master',
-      port : 12178,
+      // port : 12178,
       repo : 'git@github.com:wf-yrx/pm2-vue.git',
       path : '/home/yueyagou/www/wf',
-      'post-deploy' : 'npm pull && npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
