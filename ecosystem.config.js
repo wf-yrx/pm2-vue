9c0835922696b4c88c17ad316732f9d102b6f780
万峰 <iuliao@163.com>
2018-10-25 17:02:08 +0800

    第五次提交

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'webpack2',
-    script: 'main.js',
+    script: './main.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
@@ -22,7 +22,7 @@ module.exports = {
       user : 'yueyagou',
       host : '47.107.74.126',
       ref  : 'origin/master',
-      // port : 12178,
+      port : 12178,
       repo : 'git@github.com:wf-yrx/pm2-vue.git',
       path : '/home/yueyagou/www/wf',
       'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
